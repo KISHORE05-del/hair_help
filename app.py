cbe748470fb102a653c323981bfad5a77648eb6d
Kishore S <kishorearjun2006@gmail.com>
2026-03-16 15:28:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ person = {
     "name": "Sundar D",
     "age": 21,
     "phone": "8925392555",
-    "problem": "Temple bald area (sotta)",
-    "hospital": "Hair Transplant Clinic",
+    "problem": "Temple bald area",
+    "hospital": "Hair Clinic",
     "cost_per_graft": "₹35",
     "required_grafts": "800"
 }
@@ -17,4 +17,4 @@ def home():
     return render_template("index.html", person=person)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
